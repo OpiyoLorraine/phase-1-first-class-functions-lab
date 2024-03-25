b498a6a8353fec45d592c1991e5eaf1c7dcd0c47
Lorraine Opiyo <achienglorraine8@gmail.com>
2024-03-25 21:41:20 +0300

    phase-1-first-class-functions-lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(){
+    return (['Antonia', 'Nuru'])
+}
+
+const returnLastTwoDrivers = function(){
+    return (['Amari', 'Mo'])
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(quad){
+    return function(fare){
+        return (quad * fare)
+    }
+}
+
+const fareDoubler = createFareMultiplier(2)
+const fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers(returnFirstTwoDrivers, returnLastTwoDrivers){
+    return returnLastTwoDrivers(returnFirstTwoDrivers)
+}
